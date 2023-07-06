7d627705f5ef76bca64e99da17dd78d50276608e
Artyom Guryanov <bishop81285@gmail.com>
2023-07-06 14:27:14 +0600

    homework 2 done

--- src/item.py
+++ src/item.py
@@ -1,9 +1,16 @@
+import csv
+import json
+
+from settings import CSV_FILE_PATH
+
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
     pay_rate = 1.0
     all = []
+    __csv_path = CSV_FILE_PATH
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
@@ -13,10 +20,21 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        if len(new_name) <= 10:
+            self.__name = new_name
+        else:
+            raise Exception('Длина наименования товара превышает 10 символов.')
 
     def calculate_total_price(self) -> float:
         """
@@ -31,3 +49,31 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        """
+        Класс-метод, инициализирующий экземпляры класса Item данными из файла src/items.csv
+        """
+        with open(cls.__csv_path) as csvfile:
+            reader = csv.DictReader(csvfile)
+
+            row: dict
+            for row in reader:
+                cls(**row)
+
+    @staticmethod
+    def string_to_number(string_numb):
+        """
+        Статический метод, возвращающий число из числа-строки
+        """
+        if '.' in string_numb:
+            return int(float(string_numb))
+
+        return int(string_numb)
+
+    def __del__(self):
+        """
+        Деструктор класса Item. Вызывается перед уничтожением объекта.
+        """
+        Item.all.remove(self)
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,11 +1,15 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import csv
+
+import pytest
+
 from src.item import Item
 
 
 def test_item_init():
     item = Item("Test Item", 100.0, 5)
 
-    assert item.name == "Test Item"
+    assert item.name == "Test Item"  # Property
     assert item.price == 100.0
     assert item.quantity == 5
     assert item in Item.all
@@ -20,3 +24,43 @@ def test_apply_discount(item):
     item.apply_discount()
 
     assert item.price == 50.0
+
+
+def test_name_setter_valid_length(item):
+    item.name = 'Test Name'
+    assert item.name == 'Test Name'
+
+
+def test_name_setter_invalid_length(item):
+    with pytest.raises(Exception) as exception_info:
+        item.name = "Very Long Name"
+
+    assert str(exception_info.value) == 'Длина наименования товара превышает 10 символов.'
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
+
+
+def test_instantiate_from_csv(tmp_path):
+    csv_path = tmp_path / "test.csv"
+    with open(csv_path, "w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["name", "price", "quantity"])
+        writer.writerow(["Item1", "250", "2"])
+        writer.writerow(["Item2", "300", "10"])
+
+    Item._Item__csv_path = str(csv_path)
+    Item.all = []
+
+    Item.instantiate_from_csv()
+
+    assert len(Item.all) == 2
+    assert Item.all[0].name == "Item1"
+    assert Item.all[0].price == "250"
+    assert Item.all[0].quantity == "2"
+    assert Item.all[1].name == "Item2"
+    assert Item.all[1].price == "300"
+    assert Item.all[1].quantity == "10"
