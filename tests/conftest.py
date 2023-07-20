1cff2a4acfce295fb18baaf33ba3499c3de14c68
Artyom Guryanov <bishop81285@gmail.com>
2023-07-20 14:05:24 +0600

    add tests for Keyboard class

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 
 from src.item import Item
+from src.keyboard import Keyboard
 from src.phone import Phone
 
 
@@ -12,3 +13,8 @@ def item():
 @pytest.fixture
 def phone():
     return Phone("Test Phone", 500.0, 10, 1)
+
+
+@pytest.fixture
+def keyboard():
+    return Keyboard("Test Keyboard", 800.0, 7)
